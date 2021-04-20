03c7f23b78383ebd5fa82bddd059f28b023dfbb1
Hanna Ferris <hferris@dbq.edu>
2021-04-20 18:56:13 -0500

    code completed for ser-worker.js

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -11,9 +11,10 @@ const FILES_TO_CACHE = [
   "./js/db.js",
   "./js/index.js",
   "./index.html",
+  "./manifest.json",
 ];
 
-// installation
+// installation needed
 self.addEventListener("install", function (Event) {
   Event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -25,20 +26,57 @@ self.addEventListener("install", function (Event) {
   self.skipWaiting();
 });
 
-//activation
+//activation needed
 self.addEventListener("activate", function (Event) {
-    Event.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Got rid of old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+  Event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Got rid of old cache data", key);
+            return caches.delete(key);
+          }
         })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+//fetching needed and cache requests to api completed
+self.addEventListener("fetch", function (Event) {
+  if (Event.request.url.includes("/api")) {
+    Event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+
+        //When reciving postive response, copy and save to the cache
+        .then((cache) => {
+          return (
+            fetch(Event.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(Event.request.url, response.clone());
+                }
+                return response;
+              })
+              // if network request fails, then try grabbing it from cache
+              .catch(async (error) => {
+                return cache.match(Event.request);
+              })
+          );
+        })
+        .catch((error) => console.log(error))
     );
 
-    self.clients.claim();
+    return;
+  }
+
+  // if the request is not for the API, use "offline-first" approach
+  Event.respondWith(
+    caches.match(Event.request).then(function (response) {
+      return response || fetch(Event.request);
+    })
+  );
 });
