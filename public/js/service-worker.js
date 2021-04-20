27d22869e03e73180a98eb46c25b211080bcc297
Hanna Ferris <hferris@dbq.edu>
2021-04-20 18:08:34 -0500

    working on public dir. in serivce

---
+++ public/js/service-worker.js
@@ -0,0 +1,14 @@
+// All that are in the public directory
+const FILES_TO_CACHE = [
+    "./",
+    "./css/styles.css",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
+    // "../imgs/",
+    "./js/db.js",
+    "./js/index.js",
+    "./index.html"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
