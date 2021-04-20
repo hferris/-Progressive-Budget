64e26f794bf3411d0ac684bd55f3618a7db933a9
Hanna Ferris <hferris@dbq.edu>
2021-04-20 18:20:36 -0500

    updated eventListener for activation

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,14 +1,44 @@
-// All that are in the public directory
-const FILES_TO_CACHE = [
-    "./",
-    "./css/styles.css",
-    "./icons/icon-192x192.png",
-    "./icons/icon-512x512.png",
-    // "../imgs/",
-    "./js/db.js",
-    "./js/index.js",
-    "./index.html"
-];
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// All that are in the public directory
+const FILES_TO_CACHE = [
+  "./",
+  "./css/styles.css",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  // "../imgs/",
+  "./js/db.js",
+  "./js/index.js",
+  "./index.html",
+];
+
+// installation
+self.addEventListener("install", function (Event) {
+  Event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("the files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+//activation
+self.addEventListener("activate", function (Event) {
+    Event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Got rid of old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
